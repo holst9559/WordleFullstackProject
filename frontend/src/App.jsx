9994bbcec9a0d696442da7e95e7dbdd171389577
Anton Holst <Anton Holst>
2023-03-31 11:40:54 +0200

    Adjusted colors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,14 +8,14 @@ function App() {
 
   return (
     <div className="App h-screen w-screen bg-gray-800">
-      <header className="flex items-center flex-row h-20 border-b-2 justify-center bg-gray-950 opacity-90">
+      <header className="flex items-center flex-row h-20 border-b-2  bg-gray-800 opacity-90">
         <a
           className="font-bold text-6xl ml-auto mr-auto text-white p-10"
           href="/">
           WORDLE
         </a>
       </header>
-      <nav className="bg-gray-950 flex items-center flex-row justify-center pt-2 opacity-90">
+      <nav className="bg-gray-800 flex items-center flex-row justify-center pt-2 opacity-90">
         <a className=" text-white text-3xl pr-6" href="/highscores">
           Highscores
         </a>
@@ -23,39 +23,39 @@ function App() {
           Info
         </a>
       </nav>
-      <div className="bg-gray-800 max-h-screen max-w-screen mt-20 mb-20">
+      <div className="bg-gray-800 max-h-screen max-w-screen mt-10 mb-20">
         <div className="flex flex-row ml-auto mr-auto max-w-fit bg-gray-800 gap-3 mb-3">
-          <div className="letter h-16 w-16 border-2 bg-gray-950 text-white text-center text-5xl">
+          <div className="letter h-16 w-16 border-2  text-white text-center text-5xl">
             K
           </div>
-          <div className="letter h-16 w-16 border-2 bg-gray-950 text-white text-center text-5xl">
+          <div className="letter h-16 w-16 border-2  text-white text-center text-5xl">
             A
           </div>
-          <div className="letter h-16 w-16 border-2 bg-gray-950 text-white text-center text-5xl">
+          <div className="letter h-16 w-16 border-2  text-white text-center text-5xl">
             F
           </div>
-          <div className="letter h-16 w-16 border-2 bg-gray-950 text-white text-center text-5xl">
+          <div className="letter h-16 w-16 border-2  text-white text-center text-5xl">
             F
           </div>
-          <div className="letter h-16 w-16 border-2 bg-gray-950 text-white text-center text-5xl">
+          <div className="letter h-16 w-16 border-2  text-white text-center text-5xl">
             E
           </div>
         </div>
 
         <div className="flex flex-row ml-auto mr-auto max-w-fit bg-gray-800 gap-3 mb-3">
-          <div className="letter h-16 w-16 border-2 bg-gray-950 text-white text-center text-5xl">
+          <div className="letter h-16 w-16 border-2  text-white text-center text-5xl">
             K
           </div>
-          <div className="letter h-16 w-16 border-2 bg-gray-950 text-white text-center text-5xl">
+          <div className="letter h-16 w-16 border-2  text-white text-center text-5xl">
             A
           </div>
-          <div className="letter h-16 w-16 border-2 bg-gray-950 text-white text-center text-5xl">
+          <div className="letter h-16 w-16 border-2  text-white text-center text-5xl">
             F
           </div>
-          <div className="letter h-16 w-16 border-2 bg-gray-950 text-white text-center text-5xl">
+          <div className="letter h-16 w-16 border-2  text-white text-center text-5xl">
             K
           </div>
-          <div className="letter h-16 w-16 border-2 bg-gray-950 text-white text-center text-5xl">
+          <div className="letter h-16 w-16 border-2  text-white text-center text-5xl">
             E
           </div>
         </div>
